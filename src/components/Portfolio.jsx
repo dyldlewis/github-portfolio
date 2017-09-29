91fac30c835a4b51a077a60c98b76119ffdbf4d5
Dylan Lewis <dyldlewis@gmail.com>
2017-09-29 14:27:47 -0700

    create portfolio component

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import react from "react"
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+class Portfolio extends React.component
+
+
+
+
+
+
+
+export connect()(Portfolio);
---
+++ src/constants/api-keys.js
@@ -0,0 +1,1 @@
+export default apiKey = "56022ce857ad14dd8e1fa032d76edd8c8944d600";
